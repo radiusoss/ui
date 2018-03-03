cd1d80b83a1b6ea4fc820827ab97f2df29f47907
Eric Charles <eric@datalayer.io>
2018-03-03 09:37:24 +0100

    bubble disply

--- src/views/table/TableBubbleDisplay.tsx
+++ src/views/table/TableBubbleDisplay.tsx
@@ -23,64 +23,54 @@ export default class TableBubbleDisplay extends TableBaseDisplay {
   }
 
   private prepareBubbleData(columns, items) {
-    var labels = []
-    for (var i = 0; i < 3; i++) {
-      labels.push(columns[i]['name'])
-    }
-    var color = this.getRandomColor()
-    var datasets = {
-      label: `X=${labels[0]} Y=${labels[1]} S=${labels[2]}`,
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: color,
-      borderColor: color,
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: color,
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: color,
-      pointHoverBorderColor: color,
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10
-    }
-    var data = []
-    for (var i = 0; i < items.length; i++) {
-      data.push({
-        x: parseInt(items[i][labels[0]]),
-        y: parseInt(items[i][labels[1]]),
-        r: parseInt(items[i][labels[2]])
-      })
-    }
-    var bubbleData = {
-      labels: labels,
-      datasets: [{
-        label: 'Dataset',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: data
-      }]
-    }
-    this.bubbleData = bubbleData
-  }
 
+    if (columns.length > 1) {
+      var labels = []
+      labels.push(columns[0]['name'])
+      labels.push(columns[1]['name'])
+      var label = `X=${labels[0]} Y=${labels[1]}`
+      if (columns[2]) {
+        labels.push(columns[2]['name'])
+        label = label + ` S=${labels[2]}`
+      }
+      var color = this.getRandomColor()
+      var data = []
+      for (var i = 0; i < items.length; i++) {
+        var r = '1'
+        if (items[i][labels[2]]) {
+          r = items[i][labels[2]]
+        }
+        data.push({
+          x: parseInt(items[i][labels[0]]),
+          y: parseInt(items[i][labels[1]]),
+          r: parseInt(r)
+        })
+      }
+      var bubbleData = {
+        labels: labels,
+        datasets: [{
+          label: label,
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: data
+        }]
+      }
+      this.bubbleData = bubbleData
+    }
+  }
 }
